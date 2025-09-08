52555fc4e572314fdcbcad8b5d1bca3de3c3056b
Paulo Ferreira <paulo.mateus.f@gmail.com>
2025-09-08 16:58:37 -0300

    test: added tests

---
+++ tests/TestCase.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Lingui\Tests;
+
+use Illuminate\Filesystem\Filesystem;
+use Lingui\LinguiServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    protected Filesystem $files;
+
+    protected const LANG_PATH = __DIR__.'/Fixtures/lang';
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            LinguiServiceProvider::class,
+        ];
+    }
+
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('app.locale', 'en');
+        $app['config']->set('app.fallback_locale', 'en');
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->files = new Filesystem();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->cleanUp();
+
+        parent::tearDown();
+    }
+
+    protected function tempPath(): string
+    {
+        return __DIR__.'/temp';
+    }
+
+    protected function cleanUp(): void
+    {
+        $path = $this->tempPath();
+
+        if ($this->files->exists($path)) {
+            $this->files->deleteDirectory($path);
+        }
+    }
+}
---
+++ tests/Unit/Commands/MakeJsonTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Lingui\Tests;
+
+use Lingui\Tests\TestCase;
+
+class MakeJsonTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app->usePublicPath($this->tempPath().'/public');
+        $this->app->useLangPath(self::LANG_PATH);
+    }
+
+    public function test_success_on_create_json_files(): void
+    {
+        $this->artisan('lingui:make-json')
+            ->expectsOutput('Translation files generated in '.$this->app->publicPath().'/lang')
+            ->assertExitCode(0);
+    }
+
+    public function test_combine_php_and_json_translations(): void
+    {
+        $this->artisan('lingui:make-json');
+
+        $json = $this->files->get($this->tempPath().'/public/lang/pt.json');
+        $translations = json_decode($json, true);
+
+        $this->assertArrayHasKey('auth', $translations);
+        $this->assertEquals('A senha fornecida está incorreta.', $translations['auth']['password']);
+
+        $this->assertArrayHasKey('Hello :Name', $translations);
+        $this->assertEquals('Olá :Name', $translations['Hello :Name']);
+    }
+
+    public function test_create_specified_locale_only(): void
+    {
+        $this->artisan('lingui:make-json --locale=pt');
+
+        $generateds = $this->files->glob($this->tempPath().'/public/lang/*.json');
+
+        $this->assertEquals([$this->tempPath().'/public/lang/pt.json'], $generateds);
+    }
+}
